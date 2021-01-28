c835017ab88b751c31c9e6561e454e1297df3982
Alla <prirez@bk.ru>
2021-01-28 22:55:51 +0300

    Homework 1
      Работа над ошибками.

--- ObjC_Homework1/main.m
+++ ObjC_Homework1/main.m
@@ -7,28 +7,54 @@
 
 #import <Foundation/Foundation.h>
 
-double first = 0;
-double second = 0;
-double third = 0;
-
 int main(int argc, const char * argv[]) {
+    
     @autoreleasepool {
-        printf("First number: ");
-        scanf("%lf", &first);
-        printf("Second number: ");
-        scanf("%lf", &second);
-        printf("%lf + %lf = %lf \n",first, second, first + second);
-        printf("%lf - %lf = %lf \n",first, second, first - second);
-        printf("%lf * %lf = %lf \n",first, second, first * second);
-        printf("%lf : %lf = %lf \n",first, second, first / second);
-        printf("Add = %lf, Sub = %lf, Mul = %lf, Diff = %lf \n", first + second, first - second, first * second, first / second);
-        printf("AVG \n");
-        printf("First number: ");
-        scanf("%lf", &first);
-        printf("Second number: ");
-        scanf("%lf", &second);
-        printf("Third number: ");
-        scanf("%lf", &third);
-        printf("AVG: %lf \n", (first + second + third) / 3);
+
+        CGFloat first = 0;
+        CGFloat second = 0;
+        CGFloat third = 0;
+
+        double tmp = 0;
+
+        printf("Calculator\n");
+        printf("Please, input first number: ");
+        // It is not a good idea scan to CGFloat (it is an object),
+        // It works, because CGFloat has only one data field of type double,
+        // But it is wrong in general.
+        // See: http://macbug.ru/cocoa/stringfspec
+        scanf("%lf", &tmp);
+        first = tmp;
+
+        printf("Please, input second number: ");
+        scanf("%lf", &tmp);
+        second = tmp;
+
+        /* How to work with strings in ObjC
+        https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Strings/Articles/CreatingStrings.html
+         */
+        NSString* divResult =
+            (second != 0)
+                ? [NSString stringWithFormat:@"%lf", first / second]
+                : @"ERROR";
+
+        NSLog(@"Add = %lf, Sub = %lf, Mul = %lf, Div = %@\n",
+              first + second, first - second, first * second, divResult);
+
+        printf("\nAVG Calculator\n");
+
+        printf("Please, input first number: ");
+        scanf("%lf", &tmp);
+        first = tmp;
+
+        printf("Please, input second number: ");
+        scanf("%lf", &tmp);
+        second = tmp;
+
+        printf("Please, input third number: ");
+        scanf("%lf", &tmp);
+        third = tmp;
+
+        NSLog(@"AVG: %lf \n", (first + second + third) / 3);
     }
 }
