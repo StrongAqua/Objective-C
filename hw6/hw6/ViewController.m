ca82c39c1159c19a968043fbab75d1b8bde92ed6
Alla <prirez@bk.ru>
2021-02-15 13:50:46 +0300

    hw6 started

---
+++ hw6/hw6/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  hw6
+//
+//  Created by aprirez on 2/15/21.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
