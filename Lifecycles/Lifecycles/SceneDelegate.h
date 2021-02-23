495c973b1aa2f9852d52c43696db094508315ae5
Alla <prirez@bk.ru>
2021-02-23 17:45:53 +0300

    Homework #8
    Task #1. Создать пустое приложение и отследить все состояния его жизненного цикла с помощью вывода сообщений в консоль.
    Task #2. Отследить в созданном приложении все методы жизненного цикла контроллера.

---
+++ Lifecycles/Lifecycles/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Lifecycles
+//
+//  Created by aprirez on 2/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
